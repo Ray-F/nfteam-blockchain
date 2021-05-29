4da65c4d2534b03b8669ffeafc85cd86ea078b82
Will-ZYS <willyzhysh@gmail.com>
2021-05-29 18:51:09 +1200

    added single quotation rule in prettier con

--- server/src/routes/GalleryRoute.js
+++ server/src/routes/GalleryRoute.js
@@ -1,18 +1,18 @@
-import { Router } from "express";
-import cennznet from "../models/cennz/CennzService";
-import Approval from "../models/Approval";
+import { Router } from 'express';
+import cennznet from '../models/cennz/CennzService';
+import Approval from '../models/Approval';
 
 const router = Router();
 
 /**
  * Test example of how to use the CennzNet api client.
  */
-router.get("/test", async (req, res, next) => {
+router.get('/test', async (req, res, next) => {
   const apiClient = await cennznet.createClient();
   res.json((await cennznet.getClientDetails(apiClient)).chain);
 });
 
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   const apiClient = await cennznet.createClient();
 
   const tokenInfos = await apiClient.derive.nft.tokenInfoForCollection(0);
@@ -24,17 +24,17 @@ router.get("/", async (req, res, next) => {
     const { ipfsURL, creator, timestamp } = attributes;
 
     // tokenId, timestamp, ipfsURL, ownerAdress, VerifiedBy = []
-    const creators = dict[ipfsURL + "_" + owner][0];
+    const creators = dict[ipfsURL + '_' + owner][0];
     if (creators) {
       creators.push(creator);
     } else {
-      dict[ipfsURL + "_" + owner] = [[creator], id, timestamp];
+      dict[ipfsURL + '_' + owner] = [[creator], id, timestamp];
     }
   });
 
   let approvals = [];
   for (key in dict) {
-    const keyInfo = key.split("_");
+    const keyInfo = key.split('_');
     approvals.push(
       new Approval(
         dict[key][1],
